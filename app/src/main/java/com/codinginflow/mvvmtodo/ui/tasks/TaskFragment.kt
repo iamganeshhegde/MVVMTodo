810f20f64ea8a0f18f71d9860deb9d30b98cab55
ganeshhegdeuk <ganesh.hegde312@gmail.com>
2021-10-28 17:23:49 +0530

    MVVM todo preferences

--- app/src/main/java/com/codinginflow/mvvmtodo/ui/tasks/TaskFragment.kt
+++ app/src/main/java/com/codinginflow/mvvmtodo/ui/tasks/TaskFragment.kt
@@ -8,11 +8,15 @@ import android.view.View
 import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.codinginflow.mvvmtodo.R
+import com.codinginflow.mvvmtodo.data.SortOrder
 import com.codinginflow.mvvmtodo.databinding.FragmentTaskBinding
 import com.codinginflow.mvvmtodo.util.onQuerySearchChanged
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class TaskFragment : Fragment(R.layout.fragment_task) {
@@ -54,6 +58,10 @@ class TaskFragment : Fragment(R.layout.fragment_task) {
             viewModel.searchQuery.value = it
         }
 
+        viewLifecycleOwner.lifecycleScope.launch {
+            menu.findItem(R.id.action_hide_completed_tasks).isChecked = viewModel.preferencesFlow.first().completed
+        }
+
     }
 
 
@@ -62,18 +70,25 @@ class TaskFragment : Fragment(R.layout.fragment_task) {
         return when (item.itemId) {
             R.id.action_sort_by_name -> {
 
-                viewModel.sortOrder.value = SortOrder.BY_NAME
+//                viewModel.sortOrder.value = SortOrder.BY_NAME
+
+                viewModel.onSortOrderSelected(SortOrder.BY_NAME)
+
                 true
             }
             R.id.action_sort_by_date_created -> {
 
-                viewModel.sortOrder.value = SortOrder.BY_DATE
+//                viewModel.sortOrder.value = SortOrder.BY_DATE
+
+                viewModel.onSortOrderSelected(SortOrder.BY_DATE)
+
 
                 true
             }
             R.id.action_hide_completed_tasks -> {
                 item.isChecked = !item.isChecked
-                viewModel.hideCompleted.value = item.isChecked
+//                viewModel.hideCompleted.value = item.isChecked
+                viewModel.onHideCompleted(item.isChecked)
                 true
             }
             R.id.action_delete_all_completed_tasks -> {
--- app/src/main/java/com/codinginflow/mvvmtodo/ui/tasks/TasksViewModel.kt
+++ app/src/main/java/com/codinginflow/mvvmtodo/ui/tasks/TasksViewModel.kt
@@ -3,39 +3,54 @@ package com.codinginflow.mvvmtodo.ui.tasks
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import com.codinginflow.mvvmtodo.data.PreferencesManager
+import com.codinginflow.mvvmtodo.data.SortOrder
 import com.codinginflow.mvvmtodo.data.TaskDao
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.launch
 
 class TasksViewModel @ViewModelInject constructor(
-        private val taskDao: TaskDao
+        private val taskDao: TaskDao,
+        private val preferencesManager: PreferencesManager
 ) : ViewModel() {
 
     val searchQuery = MutableStateFlow("")
     var sortOrder = MutableStateFlow(SortOrder.BY_DATE)
+
+    val preferencesFlow = preferencesManager.preferencesFlow
 
     val hideCompleted = MutableStateFlow(false)
 
     //    private val taskFlow = searchQuery
     private val taskFlow = combine(
             searchQuery,
-            sortOrder,
-            hideCompleted
-    ) { searchQuery, sortOrder, hideCompleted ->
-        Triple(searchQuery,sortOrder, hideCompleted)
+            preferencesFlow
+//            sortOrder,
+//            hideCompleted
+    ) { searchQuery, filterPref ->
+//        Triple(searchQuery,sortOrder, hideCompleted)
+        Pair(searchQuery, filterPref)
 
-    }.flatMapLatest {(searchQuery, sortOrder, hideCompleted) ->
-                taskDao.getTasks(searchQuery, sortOrder, hideCompleted)
+//    }.flatMapLatest {(searchQuery, sortOrder, hideCompleted) ->
+    }.flatMapLatest {(searchQuery, filterPreferences) ->
+                taskDao.getTasks(searchQuery, filterPreferences.sortOrder,filterPreferences.completed)
             }
 
+
+
+    fun onSortOrderSelected(sortOrder: SortOrder) = viewModelScope.launch {
+
+        preferencesManager.updateSortOrder(sortOrder)
+    }
+
+    fun onHideCompleted(completed: Boolean) = viewModelScope.launch {
+        preferencesManager.updateHideCompleted(completed)
+    }
 
     //    val tasks = taskDao.getTasks("blah").asLiveData()
     val tasks = taskFlow.asLiveData()
 
-}
-
-enum class SortOrder {
-    BY_NAME,
-    BY_DATE
 }
